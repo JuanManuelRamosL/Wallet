7c2222f137578261257bb33019e9247e6793d483
juanmanuel <juanmanuelramoslencina@gmail.com>
2024-06-03 22:44:45 -0300

    user

--- src/app/usuario/page.jsx
+++ src/app/usuario/page.jsx
@@ -3,37 +3,44 @@ import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import "./usuario.css";
 import NavSecundario from "../../../components/nav-secundario";
+
 const Usuario = () => {
   const { error, isLoading, user } = useUser();
 
-  if (user) {
-    console.log(user);
+  if (isLoading) {
+    return <div>Cargando...</div>;
+  }
+
+  if (error) {
+    return <div>Error al cargar los datos del usuario</div>;
+  }
+
+  if (!user) {
+    return <div>No se ha iniciado sesión</div>;
   }
 
   return (
     <>
       <NavSecundario />
-      <div class="cardd">
-        <div class="infos">
-          <div class="image">
-            {" "}
-            <img src={user?.picture} alt="User Profile" />
+      <div className="cardd">
+        <div className="infos">
+          <div className="image">
+            <img src={user.picture} alt="User Profile" />
           </div>
-          <div class="info">
+          <div className="info">
             <div>
-              <h1 className="name">{user?.nickname}</h1>
-
-              <p class="function">{user?.email}</p>
+              <h1 className="name">{user.nickname}</h1>
+              <p className="function">{user.email}</p>
             </div>
-            <div class="stats">
-              <p class="flex flex-col">
+            <div className="stats">
+              <p className="flex flex-col">
                 Usuario Verificado
-                <span class="state-value">34</span>
+                <span className="state-value">34</span>
               </p>
             </div>
           </div>
         </div>
-        <button class="request" type="button">
+        <button className="request" type="button">
           <a className="button__logout" href="/api/auth/logout">
             Log Out
           </a>
