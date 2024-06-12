eeab1068ebb97d60ae9b64272a94b0963805ee80
juanmanuel <juanmanuelramoslencina@gmail.com>
2024-06-12 11:52:52 -0300

    search params dentro de use effect

--- src/app/auth/signin/page.jsx
+++ src/app/auth/signin/page.jsx
@@ -1,24 +1,23 @@
 "use client";
 import { signIn } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
 import { useForm } from "react-hook-form";
 import "./signin.css"; // Importa el archivo CSS de estilos
 
 export default function SignIn() {
-  const [error, setError] = useState(null); // No se especifica tipo
-  const searchParams = useSearchParams();
+  const [error, setError] = useState(null);
   const { register, handleSubmit } = useForm();
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      const errorParam = searchParams.get("error");
+      const urlParams = new URLSearchParams(window.location.search);
+      const errorParam = urlParams.get("error");
       if (errorParam) {
         console.error("Authentication error:", errorParam);
         setError(errorParam);
       }
     }
-  }, [searchParams]);
+  }, []);
 
   const onSubmit = handleSubmit((data) => {
     console.log(data);
